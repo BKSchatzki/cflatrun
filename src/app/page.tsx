070c79a651b35519e1965f8561173fb13d2c0798
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-12-17 06:13:20 -0800

    Add gray matter util and homepage render test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,49 @@
 import Link from 'next/link';
 
-import membersContent from '@/content/members.md';
+import {
+  getMembersData,
+  MemberData,
+} from '@/utils/graymatter';
 
-type Member = {
-  name: string;
-  position: string;
-  voicepart: string;
-  classof: string;
+type Member = MemberData;
+
+export const getStaticProps = async () => {
+  const allMembersData = getMembersData();
+  return {
+    props: {
+      members: allMembersData,
+    },
+  };
 };
 
-export default function Home() {
-  const { title, members } = membersContent.attributes;
-  const MembersContent = membersContent.react;
-
+export default function Home({ members }: { members: Member[] }) {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <header className="row-start-1 flex flex-wrap items-center justify-center gap-6">
         Header
       </header>
       <main className="row-start-2 flex w-full flex-col items-center gap-8">
-        <h1 className="text-5xl">Hello World</h1>
-        <h2 className="text-3xl">{title}</h2>
-        <MembersContent />
-        <ul className="flex w-1/2 min-w-96 flex-col gap-4">
-          {members.map((member: Member) => (
-            <li
-              key={member.name}
-              className="grid grid-cols-12 gap-4"
-            >
-              <h3 className="col-span-4">{member.name}</h3>
-              <p className="col-span-4">{member.position}</p>
-              <p className="col-span-2 text-right">{member.voicepart}</p>
-              <p className="col-span-2 text-right">{member.classof}</p>
-            </li>
-          ))}
+        <h2 className="text-3xl">Members</h2>
+        <ul className="flex w-1/2 min-w-96 flex-col gap-12">
+          {members.map(
+            (member: Member) =>
+              member.iscurrent && (
+                <li
+                  key={member.name}
+                  className="flex flex-col gap-4"
+                >
+                  <h3>{member.name}</h3>
+                  <p>{member.classof}</p>
+                  <p>{member.voicepart}</p>
+                  <p>{member.position}</p>
+                  <p>{member.bio}</p>
+                  <img
+                    src={member.image}
+                    alt={member.name}
+                  />
+                </li>
+              )
+          )}
         </ul>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
---
+++ src/utils/graymatter.ts
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
+// import { PostData } from './types'; // Import your type
+
+// example if using typescript: this is based on the post schema we created in the config.yml
+
+export interface MemberData {
+  name: string;
+  classof: string;
+  voicepart: string;
+  position?: string;
+  bio?: string;
+  image?: string;
+  iscurrent: boolean;
+}
+
+const membersDirectory = path.join(process.cwd(), 'content/members');
+
+export function getMembersData(): MemberData[] {
+  const fileNames = fs.readdirSync(membersDirectory);
+  const allMembersData = fileNames.map((fileName) => {
+    const fullPath = path.join(membersDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const { data } = matter(fileContents);
+
+    return {
+      ...data,
+    } as MemberData;
+  });
+
+  return allMembersData;
+}
+
+// export function getAllPostIds() {
+//   const fileNames = fs.readdirSync(postsDirectory);
+//   return fileNames.map((fileName) => ({
+//     params: {
+//       slug: fileName.replace(/\.md$/, ''), // Removing .md extension
+//     },
+//   }));
+// }
+
+// export function getPostData(slug: string): PostData {
+//   const fullPath = path.join(postsDirectory, `${slug}.md`);
+//   const fileContents = fs.readFileSync(fullPath, 'utf8');
+//   const matterResult = matter(fileContents);
+
+//   return {
+//     slug,
+//     ...matterResult.data,
+//     date:
+//       matterResult.data.date instanceof Date
+//         ? new Date(matterResult.data.date).toISOString()
+//         : matterResult.data.date,
+//     body: matterResult.content,
+//   } as PostData;
+// }
