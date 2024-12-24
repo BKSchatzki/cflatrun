a2fb96dd68c74eb5e1efa4aae6fbc12f79edf22b
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-12-24 15:39:29 -0800

    Decrease vertical spacing between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import Members from './_sections/Members';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-9 font-[family-name:var(--font-geist-sans)]">
-      <main className="row-start-2 flex w-full max-w-[1280px] flex-col items-center gap-[4.5rem] px-3 pb-9 sm:px-6 md:px-12">
+    <div className="flex min-h-screen flex-col items-center font-[family-name:var(--font-geist-sans)]">
+      <main className="flex w-full max-w-[1280px] flex-col items-center gap-3 px-3 pb-9 sm:px-6 md:px-12">
         <Hero />
         <Members />
         <Music />
       </main>
-      <div className="w-full max-w-[1280px] items-center px-3 pt-12 sm:px-6 md:px-12">
+      <div className="w-full max-w-[1280px] px-3 sm:px-6 md:px-12">
         <Footer />
       </div>
     </div>
